c556089e5b25e0df4b4590d689e3e81d7c59393c
mejiabrayan <learnwithbrayan@gmail.com>
2023-04-10 19:39:07 -0700

    Add navigation items with active state to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,53 @@
 'use client';
-
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { FC, useState } from 'react';
-import { motion } from 'framer-motion';
+import { LayoutGroup, motion } from 'framer-motion';
 
 interface HeaderProps {}
 
+const navItems = {
+  '/': {
+    name: 'Home',
+  },
+  '/projects': {
+    name: 'Projects',
+  },
+  '/blog': {
+    name: 'Blog',
+  },
+};
+
 const Header: FC<HeaderProps> = ({}) => {
   const [isOpen, setIsOpen] = useState(false);
+  // here we're basically saying that if the pathName is undefined, then we'll use the '/' path
+  let pathname = usePathname() || '/';
+  // if the pathname includes '/blog/' then we'll set the pathName to '/blog'
+  if (pathname.includes('/blog/')) {
+    pathname = '/blog';
+  }
 
   return (
-    <nav className='fixed top-0 left-0 bottom-0 w-50 bg-gray-910 z-50 border-r border-slate-300 dark:border-slate-700 shadow-sm flex flex-col'>
-      <div className='flex items-center justify-between h-20 border-b border-slate-300 dark:border-slate-700 px-4'>
-        <Link href='/' className='sm:hidden'>
-          ~
-        </Link>
+    <header className='flex flex-col md:flex-row bg-white dark:bg-gray-900 shadow-md items-center justify-start'>
+      <div className='flex justify-between items-center px-4 md:px-8 py-4 md:py-6 md:border-r border-gray-200 dark:border-gray-800'>
+        <motion.a
+          className='text-xl md:text-2xl font-bold tracking-tight transition-colors duration-300 hover:text-gray-700 dark:hover:text-gray-300'
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          Brayan
+        </motion.a>
+
         <button
-          className='block md:hidden focus:outline-none'
+          className='md:hidden text-3xl text-gray-600 dark:text-gray-400 focus:outline-none'
           onClick={() => setIsOpen(!isOpen)}
         >
+          <span className='sr-only'>Open menu</span>
           <svg
-            className='h-6 w-6 fill-current text-gray-700 dark:text-gray-300'
+            className='w-8 h-8 fill-current'
             viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
           >
             <path
               className={isOpen ? 'hidden' : 'block'}
@@ -38,37 +64,46 @@ const Header: FC<HeaderProps> = ({}) => {
           </svg>
         </button>
       </div>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-        className={`${
-          isOpen ? 'block' : 'hidden'
-        } md:block flex-1 flex-col items-center justify-center p-4 mt-5`}
-      >
-        <motion.a
-          whileHover={{ scale: 1.1, backgroundColor: '#4a5568' }}
-          href='https://vercel.com/docs'
-          className='text-gray-300 hover:text-white py-2 block rounded'
+      <LayoutGroup>
+        <nav
+          className={`${
+            isOpen ? 'block' : 'hidden'
+          } md:flex md:flex-row md:items-center md:justify-end md:px-8 py-4 md:py-6 w-full md:w-auto bg-white dark:bg-gray-900`}
         >
-          Docs
-        </motion.a>
-        <motion.a
-          whileHover={{ scale: 1.1, backgroundColor: '#4a5568' }}
-          href='https://vercel.com/blog'
-          className='text-gray-300 hover:text-white py-2 block rounded'
-        >
-          Blog
-        </motion.a>
-        <motion.a
-          whileHover={{ scale: 1.1, backgroundColor: '#4a5568' }}
-          href='https://vercel.com/contact'
-          className='text-gray-300 hover:text-white py-2 block rounded'
-        >
-          Contact Us
-        </motion.a>
-      </motion.div>
-    </nav>
+          {Object.entries(navItems).map(([path, { name }]) => {
+            const isActive = path === pathname;
+            return (
+              <Link
+                key={path}
+                href={path}
+                className={clsx(
+                  'transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle',
+                  {
+                    'text-neutral-500': !isActive,
+                    'font-bold': isActive,
+                  }
+                )}
+              >
+                <span className='relative py-[5px] px-[10px]'>
+                  {name}
+                  {path === pathname ? (
+                    <motion.div
+                      className='absolute inset-0 bg-neutral-100 dark:bg-neutral-800 rounded-md z-[-1]'
+                      layoutId='sidebar'
+                      transition={{
+                        type: 'spring',
+                        stiffness: 350,
+                        damping: 30,
+                      }}
+                    />
+                  ) : null}
+                </span>
+              </Link>
+            );
+          })}
+        </nav>
+      </LayoutGroup>
+    </header>
   );
 };
 
