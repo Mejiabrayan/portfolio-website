ea6cc851237b8d9c0a5c286c322fecdf93071439
mejiabrayan <learnwithbrayan@gmail.com>
2023-04-17 19:48:11 -0700

    Refactor Footer component to use the word "love" instead of the heart emoji for better accessibility and clarity

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className='container mx-auto px-4 md:px-8'>
         <nav className='flex flex-col md:flex-row justify-center items-center'>
           <div className='flex items-center mt-4 md:mt-0 sm:hidden'>
-            Made in San Francisco with ❤️ by Brayan
+            Made in San Francisco with love by Brayan
           </div>
         </nav>
       </div>
