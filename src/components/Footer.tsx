c99ff9b4b1862ea3ce3fe79b382b9a09b301dfa0
mejiabrayan <learnwithbrayan@gmail.com>
2023-04-17 19:22:14 -0700

    Add Footer component to the project for displaying credits and links in the app footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className='py-6'>
+      <div className='container mx-auto px-4 md:px-8'>
+        <nav className='flex flex-col md:flex-row justify-center items-center'>
+          <div className='flex items-center mt-4 md:mt-0 sm:hidden'>
+            Made in San Francisco with ❤️ by Brayan
+          </div>
+        </nav>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
