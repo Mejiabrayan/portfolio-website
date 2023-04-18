ad1a63bb045089b5e0b7898423a5a5cedf048cf7
mejiabrayan <learnwithbrayan@gmail.com>
2023-04-17 19:43:10 -0700

    Refactor Loading component to improve readability and add spacing

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,10 +5,11 @@ import Paragraph from '@/components/ui/Paragraph';
 const Loading = () => {
   return (
     <div className='flex flex-col items-center text-center justify-center min-h-screen '>
-        
       <LoadingAnimation />
       <div className='mt-6'>
-        <LargeHeading size='lg' className='text-gray-100 text-center'>Loading...</LargeHeading>
+        <LargeHeading size='lg' className='text-gray-100 text-center'>
+          Loading...
+        </LargeHeading>
         <Paragraph className='text-white font-nunito text-sm mt-2' size='sm'>
           Please wait while we load the content
         </Paragraph>
