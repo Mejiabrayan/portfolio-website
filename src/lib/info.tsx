7e6b754889db12b6476fa1474a5810d92ccdfdd4
mejiabrayan <learnwithbrayan@gmail.com>
2023-04-24 12:40:10 -0700

    Add info.tsx with name, avatar, title and aboutMe functions

---
+++ src/lib/info.tsx
@@ -0,0 +1,17 @@
+export const name = 'Brayan Mejia Cuenca';
+export const avatar = '/avatar.jpg';
+
+export const title = () => {
+  return <>Full-Stack Software Engineer</>;
+};
+
+
+
+export const aboutMe = () => {
+  return (
+    <>
+      I&apos;m passionate about technology, music and building things on the
+      web.
+    </>
+  );
+};
